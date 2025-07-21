ab102d193300a73105874eedd73ef5776677be05
krykox <liliancaffier14@gmail.com>
2025-07-21 14:18:31 +0200

    Add google analytics :chart_with_upwards_trend:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,49 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
-import {Providers} from "./providers";
+import { Providers } from "./providers";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
+const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
+const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
 
 export const metadata: Metadata = {
     title: "Lilian Caffier",
-    description: "Hello, my name is Lilian Caffier, I’m 21 years old, and I’m currently a higher education student specializing in fullstack web development.",
+    description:
+        "Hello, my name is Lilian Caffier, I’m 21 years old, and I’m currently a higher education student specializing in fullstack web development.",
 };
 
-export default function RootLayout({children,}: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-        <body
-            className={`${geistSans.variable} ${geistMono.variable}  bg-background/70 antialiased`}
-        >
-        <Providers>
-            {children}
-        </Providers>
+        {GTM_ID && (
+            <Script id="gtm-script" strategy="afterInteractive">
+                {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+            </Script>
+        )}
+        
+        <body className={`${geistSans.variable} ${geistMono.variable} bg-background/70 antialiased`}>
+        {GTM_ID && (
+            <noscript>
+                <iframe
+                    src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                    height="0"
+                    width="0"
+                    style={{ display: "none", visibility: "hidden" }}
+                />
+            </noscript>
+        )}
+        
+        <Providers>{children}</Providers>
         </body>
         </html>
     );
